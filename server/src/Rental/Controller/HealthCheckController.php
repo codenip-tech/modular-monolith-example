2b7135d64f772bffb180512017515ca61729fe9e
Juan González <juangonzalezmontes@gmail.com>
2022-09-13 10:47:58 +0200

    feature/refactor-and-improvements
    - refactor folder structure
    - update configs

--- server/src/Rental/Controller/HealthCheckController.php
+++ server/src/Rental/Controller/HealthCheckController.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Rental\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HealthCheckController
+class HealthCheckController extends AbstractController
 {
+    #[Route('/health-check', name: 'rental_health_check', methods: ['GET'], priority: 10)]
     public function __invoke(): Response
     {
-        return new JsonResponse(['message' => 'Module Rental up and running!']);
+        return $this->json(['message' => 'Module Rental up and running!']);
     }
 }
